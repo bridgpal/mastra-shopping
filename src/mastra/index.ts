a10a222d026f09747c0efbcf89347d6e9ced0273
Anil Bridgpal <bridgpal@gmail.com>
2025-05-07 00:54:53 -0400

    added new memory syntax

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,7 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { NetlifyDeployer } from '@mastra/deployer-netlify';
-
+import { LibSQLStore } from '@mastra/libsql';
 import { shoppingAgent } from './agents';
 
 //Deployer stub has to be here for netlify
@@ -10,6 +10,9 @@ export const mastra = new Mastra({
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
+  }),
+  storage: new LibSQLStore({
+    url: 'file:../mastra.db',
   }),
   deployer: new NetlifyDeployer({
     scope: 'your-team-slug',
