ad6d915200e72255db0a651880f1f85ded2b0575
Anil Bridgpal <bridgpal@gmail.com>
2025-05-07 00:36:47 -0400

    trying to fix this

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,12 +1,19 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
+import { NetlifyDeployer } from '@mastra/deployer-netlify';
 
 import { shoppingAgent } from './agents';
 
+//Deployer stub has to be here for netlify
 export const mastra = new Mastra({
   agents: { shoppingAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
   }),
+  deployer: new NetlifyDeployer({
+    scope: 'your-team-slug',
+    projectName: 'your-project-name',
+    token: 'your-netlify-token'
+  })
 });
